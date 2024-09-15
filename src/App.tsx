73bdd0bd9d632dd594edcca256f1ad0e9792de9a
Vivi Poit <vwpoit@gmail.com>
2024-09-15 15:57:23 -0400

    update App and main to basic idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="row title-row">
+        <h1>Piker</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="row top-row">
+        <div>
+          <h2>Total Invested Amount</h2>
+          <p>$ 6,000,000.00</p>
+        </div>
+        <div>
+          <h2>Number of Investments</h2>
+          <p>784</p>
+        </div>
+        <div>
+          <h2>Rate of Return</h2>
+          <p>38%</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <div className="row chart-row">
+        <div>
+          <h2>Yearly Total Investment</h2>
+          <ul>
+            <li>2019: $ 1,000,000.00</li>
+            <li>2020: $ 2,000,000.00</li>
+            <li>2021: $ 3,000,000.00</li>
+            <li>2022: $ 4,000,000.00</li>
+            <li>2023: $ 5,000,000.00</li>
+            <li>2024: $ 6,000,000.00</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Total Investment by Sector</h2>
+          <ul>
+            <li>IT: $ 1,000,000.00</li>
+            <li>Healthcare: $ 1,000,000.00</li>
+            <li>Energy: $ 500,000.00</li>
+            <li>Utilities: $ 500,000.00</li>
+            <li>Finance: $ 1,000,000.00</li>
+            <li>Industrials: $ 1,000,000.00</li>
+            <li>Consumer Staples: $ 500,000.00</li>
+            <li>Consumer Discretionary: $ 500,000.00</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Total Investment by Country</h2>
+          <ul>
+            <li>USA: $ 1,000,000.00</li>
+            <li>England: $ 1,000,000.00</li>
+            <li>Australia: $ 500,000.00</li>
+            <li>France: $ 500,000.00</li>
+            <li>Germany: $ 1,000,000.00</li>
+            <li>India: $ 1,000,000.00</li>
+            <li>Brazil: $ 500,000.00</li>
+            <li>China: $ 500,000.00</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Top Investments</h2>
+          <ul>
+            <li>GOOGL: $ 1,000,000.00</li>
+            <li>DIS: $ 1,000,000.00</li>
+            <li>MCD: $ 500,000.00</li>
+            <li>NKE: $ 500,000.00</li>
+            <li>WMT: $ 1,000,000.00</li>
+            <li>PEP: $ 1,000,000.00</li>
+            <li>BRK.B: $ 500,000.00</li>
+            <li>BAC: $ 500,000.00</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
