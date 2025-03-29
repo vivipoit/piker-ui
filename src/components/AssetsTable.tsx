bb587c6238332092f2089919d2853bd556d2d75b
Vivi Poit <vwpoit@gmail.com>
2025-03-29 13:21:27 -0400

    sort columns

--- src/components/AssetsTable.tsx
+++ src/components/AssetsTable.tsx
@@ -1,4 +1,8 @@
-import { Paper, Title, Flex, Table, NumberFormatter } from '@mantine/core'
+import { Paper, Title, Flex, Table, NumberFormatter, Anchor } from '@mantine/core'
+import { IconSortAscendingLetters, IconSortAscendingNumbers } from '@tabler/icons-react';
+import { useState } from "react";
+
+const UNSORTED_COLUMN_ICON_COLOR = 'gray.3'
 
 export const AssetsTable: React.FC = () => {
   const title = "Assets"
@@ -12,6 +16,24 @@ export const AssetsTable: React.FC = () => {
     { ticker: 'PEP', sector: 'Consumer Staples', country: 'USA', latestPrice: 11.23 },
     { ticker: 'BAC', sector: 'Financials', country: 'USA', latestPrice: 7.48 },
   ]
+  const [sortedAssets, setSortedAssets] = useState(assets);
+  const [sortedByColumn, setSortedByColumn] = useState('');
+  
+  const sortAssets = (column: keyof typeof assets[0]) => {
+    const sorted = [...sortedAssets].sort((a, b) => {
+      const valA = a[column];
+      const valB = b[column];
+
+        if (typeof valA === "string" && typeof valB === "string") {
+          return valA.localeCompare(valB);
+        }
+
+        return (valA as number) - (valB as number);
+    });
+
+    setSortedByColumn(column)
+    setSortedAssets(sorted)
+  }
   
   return(
     <Paper shadow="sm" radius="md" withBorder p="xl">
@@ -21,14 +43,42 @@ export const AssetsTable: React.FC = () => {
       <Table striped highlightOnHover withRowBorders={false}>
         <Table.Thead>
           <Table.Tr>
-            <Table.Th><Flex justify="Center">Ticker</Flex></Table.Th>
-            <Table.Th><Flex justify="Center">Sector</Flex></Table.Th>
-            <Table.Th><Flex justify="Center">Country</Flex></Table.Th>
-            <Table.Th><Flex justify="Flex-end">Latest Price</Flex></Table.Th>
+            <Table.Th>
+              <Flex justify="Center">
+                Ticker
+                <Anchor component="button" c={sortedByColumn !== 'ticker' ? UNSORTED_COLUMN_ICON_COLOR : ''} onClick={() => sortAssets('ticker')}>
+                  <IconSortAscendingLetters />
+                </Anchor>
+              </Flex>
+            </Table.Th>
+            <Table.Th>
+              <Flex justify="Center">
+                Sector
+                <Anchor component="button" c={sortedByColumn !== 'sector' ? UNSORTED_COLUMN_ICON_COLOR : ''} onClick={() => sortAssets('sector')}>
+                  <IconSortAscendingLetters />
+                </Anchor>
+              </Flex>
+            </Table.Th>
+            <Table.Th>
+              <Flex justify="Center">
+                Country
+                <Anchor component="button" c={sortedByColumn !== 'country' ? UNSORTED_COLUMN_ICON_COLOR : ''} onClick={() => sortAssets('country')}>
+                  <IconSortAscendingLetters />
+                </Anchor>
+              </Flex>
+            </Table.Th>
+            <Table.Th>
+              <Flex justify="Flex-end">
+                Latest Price
+                <Anchor component="button" c={sortedByColumn !== 'latestPrice' ? UNSORTED_COLUMN_ICON_COLOR : ''} onClick={() => sortAssets('latestPrice')}>
+                  <IconSortAscendingNumbers />
+                </Anchor>
+              </Flex>
+            </Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>
-          { assets.map((asset) => 
+          { sortedAssets.map((asset) => 
             <Table.Tr key={asset.ticker}>
               <Table.Td><Flex justify="Center">{asset.ticker}</Flex></Table.Td>
               <Table.Td><Flex justify="Center">{asset.sector}</Flex></Table.Td>
