f942517b13b740c5651c9c50f0028593c1509bad
Vivi Poit <vwpoit@gmail.com>
2025-03-29 15:26:09 -0400

    add new attribute to assets data

--- src/components/AssetsTable.tsx
+++ src/components/AssetsTable.tsx
@@ -4,14 +4,14 @@ import { SortableTable } from './SortableTable/SortableTable';
 export const AssetsTable: React.FC = () => {
   const title = "Assets"
   const assets = [
-    { ticker: 'GOOGL', sector: 'Communication Services', country: 'USA', latestPrice: 123.45 },
-    { ticker: 'BRK.B', sector: 'Financials', country: 'USA', latestPrice: 1546.78 },
-    { ticker: 'DIS', sector: 'Communication Services', country: 'USA', latestPrice: 23.21 },
-    { ticker: 'MCD', sector: 'Consumer Discretionary', country: 'USA', latestPrice: 74.32 },
-    { ticker: 'NKE', sector: 'Consumer Discretionary', country: 'USA', latestPrice: 143.67 },
-    { ticker: 'WMT', sector: 'Consumer Staples', country: 'USA', latestPrice: 233.98 },
-    { ticker: 'PEP', sector: 'Consumer Staples', country: 'USA', latestPrice: 11.23 },
-    { ticker: 'BAC', sector: 'Financials', country: 'USA', latestPrice: 7.48 },
+    { ticker: 'GOOGL', sector: 'Communication Services', country: 'USA', averagePrice: 123.40, latestPrice: 123.45 },
+    { ticker: 'BRK.B', sector: 'Financials', country: 'USA', averagePrice: 1123.45, latestPrice: 1546.78 },
+    { ticker: 'DIS', sector: 'Communication Services', country: 'USA', averagePrice: 13.45, latestPrice: 23.21 },
+    { ticker: 'MCD', sector: 'Consumer Discretionary', country: 'USA', averagePrice: 103.44, latestPrice: 74.32 },
+    { ticker: 'NKE', sector: 'Consumer Discretionary', country: 'USA', averagePrice: 123.38, latestPrice: 143.67 },
+    { ticker: 'WMT', sector: 'Consumer Staples', country: 'USA', averagePrice: 209.88, latestPrice: 233.98 },
+    { ticker: 'PEP', sector: 'Consumer Staples', country: 'USA', averagePrice: 3.97, latestPrice: 11.23 },
+    { ticker: 'BAC', sector: 'Financials', country: 'USA', averagePrice: 6.56, latestPrice: 7.48 },
   ]
   
   return(
